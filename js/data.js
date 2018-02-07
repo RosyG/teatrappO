8f311260a8bf105da10410c90fe89eda68e1a5c8
8bril <mancusoo@gmail.com>
2018-02-07 15:18:06 -0600

    update

---
+++ js/data.js
@@ -0,0 +1,98 @@
+var teatros = [
+    {
+		"name": "Foro Shakespeare",
+		"img": "assets/img/",
+        "address": "Zamora 7, Condesa, 06140 Ciudad de México, CDMX",
+        "coordinates": {
+            lng: 19.4194399,
+		    lat: -99.176641
+        }
+    },
+    {
+		"name": "Orientacion",
+		"img": "assets/img/",
+        "address": "Paseo de la Reforma y Campo Marte, Paseo de la Reforma, Polanco V Sección, 11560 Ciudad de México, CDMX",
+        "coordinates": {
+            lng: -99.1954078,
+		    lat: 19.4230706
+        }
+    },
+    {
+		"name": "Centro Cultural Carretera 15 Teatro",
+		"img": "assets/img/",
+        "address": "Colima 268, Roma Nte., 06700 Ciudad de México, CDMX",
+        "category": "Mexicana",
+		"price": "$$$",
+        "coordinates": {
+            lng: -99.1640023,
+			lat: 19.4192023
+        }
+    },
+    {
+		"name": "Centro Cultural el Foco",
+		"img": "assets/img/",
+        "address": "Orizaba 87, Roma Nte., 06700 Ciudad de México, CDMX",
+        "category": "Hamburguesería",
+		"price": "$$$",
+        "coordinates": {
+            lng: -99.1611437,
+		    lat: 19.4193863
+        }
+    },
+    {
+		"name": "Centro Cultural I",
+		"img": "assets/img/",
+        "address": "Orizaba s/n, Roma Norte, Roma Nte., 06700 Cuauhtémoc, CDMX",
+        "category": "Vegana",
+		"price": "$$",
+        "coordinates": {
+            lng: -99.1610123,
+		    lat: 19.4197784
+        }
+    },
+    {
+		"name": "Centro Cultural Roldán Sandoval",
+		"img": "assets/img/",
+        "address": "Orizaba y Colima, Roma Norte, Cuauhtémoc, 06700 Ciudad de México, D.F., Roma Nte., 06700 Ciudad de México, CDMX",
+        "category": "Burritos",
+		"price": "$",
+        "coordinates": {
+            lng: -99.1606958,
+		    lat: 19.41986
+		}
+	}
+    {
+        "name": "Centro Cultural Sylvia Pasquel",
+        "img": "assets/img/",
+        "address": "Orizaba y Colima, Roma Norte, Cuauhtémoc, 06700 Ciudad de México, D.F., Roma Nte., 06700 Ciudad de México, CDMX",
+        "category": "Burritos",
+        "price": "$",
+        "coordinates": {
+            lng: -99.1606958,
+            lat: 19.41986
+        }
+    }
+    {
+        "name": "Foro 37",
+        "img": "assets/img/",
+        "address": "Orizaba y Colima, Roma Norte, Cuauhtémoc, 06700 Ciudad de México, D.F., Roma Nte., 06700 Ciudad de México, CDMX",
+        "category": "Burritos",
+        "price": "$",
+        "coordinates": {
+            lng: -99.1606958,
+            lat: 19.41986
+        }
+    }
+    {
+        "name": "Teatro Cabaret",
+        "img": "assets/img/",
+        "address": "Orizaba y Colima, Roma Norte, Cuauhtémoc, 06700 Ciudad de México, D.F., Roma Nte., 06700 Ciudad de México, CDMX",
+        "category": "Burritos",
+        "price": "$",
+        "coordinates": {
+            lng: -99.1606958,
+            lat: 19.41986
+        }
+    }
+
+]
