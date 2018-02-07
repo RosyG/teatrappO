bf774f464870ae0931ea5d837cda82f015fdc7b2
8bril <mancusoo@gmail.com>
2018-02-07 17:22:37 -0600

    update data

---
+++ js/data.js
@@ -0,0 +1,102 @@
+var teatros = [
+    {
+		"name": "Foro Shakespeare",
+		"img": "http://dummyimage.com/250x250",
+        "address": "Zamora 7, Condesa, 06140 Ciudad de México, CDMX",
+        "coordinates": {
+            lng: 19.4194399,
+		    lat: -99.176641
+        }
+    },
+    {
+		"name": "Orientacion",
+		"img": "http://dummyimage.com/250x250",
+        "address": "Paseo de la Reforma y Campo Marte, Paseo de la Reforma, Polanco V Sección, 11560 Ciudad de México, CDMX",
+        "coordinates": {
+            lng: -99.1954078,
+		    lat: 19.4230706
+        }
+    },
+    {
+		"name": "NH",
+		"img": "http://dummyimage.com/250x250",
+        "address": "Calle Liverpool 155, Juárez, 06600 Ciudad de México, CDMX",
+        "coordinates": {
+            lng: -99.166989,
+			lat: 19.4243557
+        }
+    },
+    {
+		"name": "Hélenico",
+		"img": "http://dummyimage.com/250x250",
+        "address": "Av. Revolución 1500, Guadalupe Inn, 01020 Ciudad de México, CDMX",
+        "coordinates": {
+            lng: -99.1921459,
+		    lat: 19.3525671
+        }
+    },
+    {
+		"name": "Foro Lucerna",
+		"img": "http://dummyimage.com/250x250",
+        "address": "Calle Lucerna 64, Delegación Cuauhtémoc, Col. Juárez, 06600 Ciudad de México, CDMX",
+        "coordinates": {
+            lng: -99.1589174,
+		    lat: 19.4299826
+        }
+    },
+    {
+		"name": "Bar El Vicio",
+		"img": "http://dummyimage.com/250x250",
+        "address": "Calle Madrid 13, Del Carmen, 04100 Ciudad de México, CDMX",
+        "coordinates": {
+            lng: -99.1663782,
+		    lat: 19.357465
+		}
+	}
+    {
+        "name": "Centro Cultural Universitario",
+        "img": "http://dummyimage.com/250x250",
+        "address": "Av. Ricardo Flores Magón 1, Tlatelolco, 06995 Ciudad de México, CDMX",
+        "coordinates": {
+            lng: -99.1721293,
+            lat: 19.4498291
+        }
+    }
+    {
+        "name": "Julio Prieto",
+        "img": "http://dummyimage.com/250x250",
+        "address": "Xola 809, Col del Valle Nte, 03100 Ciudad de México, CDMX",
+        "coordinates": {
+            lng: -99.1633715,
+            lat: 19.3960374
+        }
+    }
+    {
+        "name": "Foro de las Artes",
+        "img": "http://dummyimage.com/250x250",
+        "address": "Av. Río Churubusco 79, Country Club Churubusco, 04220 Ciudad de México, CDMX",
+        "coordinates": {
+            lng: -99.1429351,
+            lat: 19.3564583
+        }
+    }
+     {
+        "name": "Salvador Novo",
+        "img": "http://dummyimage.com/250x250",
+        "address": "Av. Río Churubusco 79, Country Club Churubusco, 04220 Ciudad de México, CDMX, México",
+        "coordinates": {
+            lng: -99.139786,
+            lat: 19.360311
+        }
+    }
+     {
+        "name": "Santa Catarina",
+        "img": "http://dummyimage.com/250x250",
+        "address": "Jardín Sta Catarina 10, Santa Catarina, 04010 Ciudad de México, CDMX",
+        "coordinates": {
+            lng: -99.1711436,
+            lat: 19.3498174
+        }
+    }
+
+]
